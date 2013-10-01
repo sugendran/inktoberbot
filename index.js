a809ceb3a8f82ed6987750f6e6e30d6c7e1b4003
Sugendran Ganess <sugendran@sugendran.net>
2013-10-02 08:19:30 +1000

    updated fork for inktober

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-// config is using environment vars so that 
+// config is using environment vars so that
 // sensitive info is not in the repo
 var conf = {
-	keys: {
-	    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-	    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-	},
-	terms: ['nodejs']
+    keys: {
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    },
+    terms: ['inktober']
 };
 
 // going to use the tuiter node module to get access to twitter
 var tu = require('tuiter')(conf.keys);
 
-// what to do after we retweet something. 
-// if it fails i really don't care unless 
+// what to do after we retweet something.
+// if it fails i really don't care unless
 // i'm debugging
+
 function onReTweet(err) {
-    if(err) {
+    if (err) {
         console.error("retweeting failed :(");
         console.error(err);
     }
 }
 
 // what to do when we get a tweet
+
 function onTweet(tweet) {
     // if it's flagged as a retweet or has RT
-    // in there then we probably don't want 
+    // in there then we probably don't want
     // to retweet it again.
     if (tweet.retweeted) {
         return;
@@ -34,7 +36,7 @@ function onTweet(tweet) {
     if (tweet.text.indexOf("RT") !== -1) {
         return;
     }
-    console.log("Retweeting: " + tweet.text);
+    // console.log("Retweeting: " + tweet.text);
     // note we're using the id_str property since
     // javascript is not accurate for 64bit ints
     tu.retweet({
